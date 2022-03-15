ca1620507bddea058ce5efcf7497d1dbbdf8a152
Diego Alessandro <diegoalessandrodacruzmartins@gmail.com>
2022-03-15 16:21:01 -0300

    feat: create movie components

---
+++ src/components/row.tsx
@@ -0,0 +1,96 @@
+import { Box, Flex, Heading, IconButton, Image } from "@chakra-ui/react"
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useState } from "react"
+
+export default function Row({ movies, label }){
+  const [scrollX, setScrollX] = useState(-400)
+
+  function handleLeftArrow(){
+    let x = scrollX + Math.round(window.innerWidth / 2)
+    if(x > 0){
+      x = 0
+    }
+
+    setScrollX(x)
+  }
+
+  function handleRightArrow(){
+    let x = scrollX - Math.round(window.innerWidth / 2)
+    let totalWidth = movies.length * 150
+    if((window.innerWidth - totalWidth) > x){
+      x = (window.innerWidth - totalWidth) - 5
+    }
+
+    setScrollX(x)
+  }
+
+  return (
+    <Box w="100vw" h="280px">
+      <Heading 
+        mb="15px" 
+        mt="20px" 
+        ml="3rem" 
+        as="h2" 
+        size="md"
+      >
+        {label}
+      </Heading>
+      <IconButton 
+        icon={<FontAwesomeIcon icon={faArrowLeft} />} 
+        aria-label="mover para esquerda"
+        left="0"
+        h="230px"
+        bgColor="#00000070"
+        pos="absolute"
+        zIndex={9}
+        onClick={handleLeftArrow}
+        borderRadius="none"
+        _hover={{
+          bgColor: "#00000090"
+        }}
+      />
+      <IconButton 
+        icon={<FontAwesomeIcon icon={faArrowRight} />} 
+        aria-label="mover para direita"
+        right="0"
+        h="230px"
+        bgColor="#00000070"
+        pos="absolute"
+        zIndex={9}
+        onClick={handleRightArrow}
+        borderRadius="none"
+        _hover={{
+          bgColor: "#00000090"
+        }}
+      />
+      <Flex 
+        pos="relative" 
+        flexDir="row" 
+        w={movies.length * 150} 
+        marginLeft={scrollX} 
+        flexShrink="unset"
+        transition="ease-in-out .3s"
+      >
+        {movies.map(movie => { 
+            const poster_path = "https://image.tmdb.org/t/p/w200" + movie.poster_path
+            return (
+              <Image 
+                width="150px" 
+                height="230px"
+                src={poster_path} 
+                alt={movie.title} 
+                key={movie.id}
+                transition=".3s"
+                _hover={{
+                  transform: "scale(1.3)",
+                  borderRadius: "sm"
+                }}
+              />
+            )
+          })
+        }   
+      </Flex>
+    </Box>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,60 @@
 import { Flex } from "@chakra-ui/react"
+import { GetStaticProps } from "next"
+import Row from "../components/row"
+import TMDb from "../lib/TMDb"
 
-export default function Home() {
+export default function Home({ 
+  movieTheater,
+  horror,
+  action,
+  adventure,
+  comedy,
+  scienceFiction,
+  drama,
+  romance,
+  fantasy,
+  documentary
+}) {
   return (
-    <Flex>
-      
+    <Flex flexDirection="column"> 
+      <Row label="Em cartaz" movies={movieTheater.results} />
+      <Row label="Terror" movies={horror.results} />
+      <Row label="Ação" movies={action.results} />
+      <Row label="Aventura" movies={adventure.results} />
+      <Row label="Comédia" movies={comedy.results} />
+      <Row label="Ficção cientifica" movies={scienceFiction.results} />
+      <Row label="Drama" movies={drama.results} />
+      <Row label="Romance" movies={romance.results} />
+      <Row label="Fantasia" movies={fantasy.results} />
+      <Row label="Documentario" movies={documentary.results} />
     </Flex>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data: movieTheater } = await TMDb.get(`movie/now_playing?api_key=${process.env.TMDB_KEY}&language=pt-BR&page=1&region=br`)
+  const { data: horror } = await TMDb.get(`discover/movie?with_genres=27&language=pt-BR&api_key=${process.env.TMDB_KEY}`)
+  const { data: action } = await TMDb.get(`discover/movie?with_genres=28&language=pt-BR&api_key=${process.env.TMDB_KEY}`)
+  const { data: adventure } = await TMDb.get(`discover/movie?with_genres=12&language=pt-BR&api_key=${process.env.TMDB_KEY}`)
+  const { data: comedy } = await TMDb.get(`discover/movie?with_genres=35&language=pt-BR&api_key=${process.env.TMDB_KEY}`)
+  const { data: scienceFiction } = await TMDb.get(`discover/movie?with_genres=878&language=pt-BR&api_key=${process.env.TMDB_KEY}`)
+  const { data: drama } = await TMDb.get(`discover/movie?with_genres=18&language=pt-BR&api_key=${process.env.TMDB_KEY}`)
+  const { data: romance } = await TMDb.get(`discover/movie?with_genres=10749&language=pt-BR&api_key=${process.env.TMDB_KEY}`)
+  const { data: fantasy } = await TMDb.get(`discover/movie?with_genres=14&language=pt-BR&api_key=${process.env.TMDB_KEY}`)
+  const { data: documentary } = await TMDb.get(`discover/movie?with_genres=99&language=pt-BR&api_key=${process.env.TMDB_KEY}`)
+
+  return {
+    props: {
+      movieTheater,
+      horror,
+      action,
+      adventure,
+      comedy,
+      scienceFiction,
+      drama,
+      romance,
+      fantasy,
+      documentary
+    }
+  }
 }
